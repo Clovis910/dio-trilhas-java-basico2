b61670a85cbbb5cb5310ee6551fb449db769a2a2
Clovis <clovis910@hotmail.com>
2025-06-18 14:28:22 -0300

    atualizando

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,59 @@
+public class SistemaMedida {
+
+    public static void main(String[] args) {
+        
+        String sigla = "A";
+
+        // if(sigla == "P")
+        // System.out.println("PEQUENO");
+
+        // else if(sigla == "M")
+        // System.out.println("MEDIO");
+
+        // else if(sigla == "G")
+        // System.out.println("GRANDE");
+
+        // else
+        // System.out.println("INDEFINIDO");
+
+
+        // switch (sigla) {
+        //     case "P":
+        //         System.out.println("PEQUENO");
+        //         break;
+
+        //     case "M":
+        //         System.out.println("MEDIO");
+        //         break;
+
+        //     case "G":
+        //         System.out.println("GRANDE");
+        //         break;
+                
+        
+        //     default:
+        //         System.out.println("INDEFINIDO");
+        //         break;
+        // }
+
+        String plano = "T";
+        switch (plano) {
+            case "T":
+                System.out.println("5Gb YouTube");
+              
+        
+            case "M":
+                System.out.println("Whatsap e Instagram Gratis");
+              
+
+
+            case "B":
+                System.out.println("100 min e ligacçoes");
+               
+        }
+
+
+
+    }
+
+}
